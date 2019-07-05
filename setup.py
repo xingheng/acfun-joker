d2630bfd337c6af4dad4311c4367d3e2b0d1094b
Will Han <xingheng.hax@qq.com>
2019-07-05 17:24:08 +0800

    Version 0.2.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='acfun',
-    version='0.1',
+    version='0.2',
     author='Will Han',
     author_email='xingheng.hax@qq.com',
     license='MIT',
